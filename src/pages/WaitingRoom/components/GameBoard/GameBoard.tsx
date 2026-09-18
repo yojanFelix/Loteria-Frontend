c144e3d8bcb0dd2bcfa6f9295afe4a5f15471405
DualyRF <dualyrf@gmail.com>
2026-09-18 13:56:20 -0700

    refactor: remove Web Audio API from GameBoard to prevent browser freezing on rapid clicks

--- src/pages/WaitingRoom/components/GameBoard/GameBoard.tsx
+++ src/pages/WaitingRoom/components/GameBoard/GameBoard.tsx
@@ -79,34 +79,7 @@ export default function GameBoard({ code }: GameBoardProps) {
       }
     }
 
-    if (!isMarked) {
-      try {
-        const AudioContextClass = window.AudioContext || (window as any).webkitAudioContext;
-        if (AudioContextClass) {
-          const audioCtx = new AudioContextClass();
-          const oscillator = audioCtx.createOscillator();
-          const gainNode = audioCtx.createGain();
-          
-          oscillator.type = 'sine';
-          oscillator.frequency.setValueAtTime(800, audioCtx.currentTime);
-          oscillator.frequency.exponentialRampToValueAtTime(300, audioCtx.currentTime + 0.1);
-          
-          gainNode.gain.setValueAtTime(0.5, audioCtx.currentTime);
-          gainNode.gain.exponentialRampToValueAtTime(0.01, audioCtx.currentTime + 0.1);
-          
-          oscillator.connect(gainNode);
-          gainNode.connect(audioCtx.destination);
-          
-          oscillator.onended = () => {
-            audioCtx.close().catch(() => {});
-          };
-          
-          oscillator.start();
-          oscillator.stop(audioCtx.currentTime + 0.1);
-        }
-      } catch (e) {
-      }
-    }
+
 
     try {
       localStorage.setItem(`marcadas_${code}`, JSON.stringify(nuevo));
